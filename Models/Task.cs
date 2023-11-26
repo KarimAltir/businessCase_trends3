9cf73563007cfa4bd962f06eb5de191ebad43c79
Karim Altir <mhd.altir@student.ehb.be>
2023-11-26 12:23:21 +0100

    Create User & Task Model

---
+++ Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buisnessCase_trends3.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public int Points { get; set; }
+
+        public Task()
+        {
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buisnessCase_trends3.Models
+{
+    public class User
+    {
+        //generate a number of variables for the user. besides the obvious Id, username and password, we must also need a point counter.   
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Points { get; set; } = 0;
+
+        public int LastLeaderboardObtainedPoints { get; set; } = 0;
+        //client side
+        [NotMapped]
+        public int Rank { get; set; } = 0;
+    }
+}
