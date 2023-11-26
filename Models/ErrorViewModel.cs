aa94c5d4b1aaa2c4354ce47e053a82a74a7dde4d
Karim Altir <mhd.altir@student.ehb.be>
2023-11-26 12:18:48 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace buisnessCase_trends3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
